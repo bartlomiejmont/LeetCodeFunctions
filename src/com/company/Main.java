65ffe3c14bb30dca39be04e2fc3d4e3f6cccfaf5
SpankerTroll <spankertroll77@gmail.com>
2019-01-26 14:00:07 +0100

    working TwoSum

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        TwoSum twoSum = new TwoSum();
+
+        int[] nums = new int[]{3,6,3};
+        int target = 6;
+
+        int[] result = twoSum.twoSum(nums,target);
+        
+        System.out.println(result[0]);
+        System.out.println(result[1]);
 
     }
 }
--- src/com/company/TwoSum.java
+++ src/com/company/TwoSum.java
@@ -4,7 +4,21 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
 
         int[] result = new int[2];
+        int st =0, nd=1;
 
+        loops:
+        while(true){
+            while(nd!=nums.length){
+                if((nums[st]+nums[nd])==target){
+                    break loops;
+                }
+                nd++;
+            }
+            st++;
+            nd=st+1;
+        }
+        result[0]=st;
+        result[1]=nd;
 
         return result;
     }
